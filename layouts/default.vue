a95e3ab7c08946c46ecfff34592eff131e2029a4
Lakomka2204 <deniskozachok22@gmail.com>
2024-01-04 12:01:14 +0200

    minimal plugin setup

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav>
+        <NuxtLink to="/">CSIJAC</NuxtLink>
+        <ul>
+            <li>
+                <NuxtLink to="/about">{{ $t("home") }}</NuxtLink>
+            </li>
+        </ul>
+    </nav>
+    <slot />
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped src="./layout.css"/>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    sex
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
